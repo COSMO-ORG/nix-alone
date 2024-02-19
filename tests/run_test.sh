0ce33d0cf7665e714f7e450e77b2f6cdffe5d1c8
Nander Wever <nander.wever@meteoswiss.ch>
2024-02-19 13:58:43 +0100

    Retrieve IMIS WFJ2 data for validation of the simulations, and running SNOWPACK at both 900s and 20s resolution.

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,6 +2,10 @@ resolutions="900 20"                                    # List of temporal resol
 
 
 runSimulation() {
+    #
+    # Run NIX
+    #
+
     nsteps=$(wc -l ${stnfile} | cut -d' ' -f1)
 
     pushd ../
@@ -20,6 +24,30 @@ runSimulation() {
     popd
 
     ../nix > ${stnfile}.out
+
+
+
+    #
+    # Run SNOWPACK
+    #
+
+    metfile=$(awk 'BEGIN {input=0} {if(input) {if(/METEOPATH/) {path=$NF}; if(/STATION1/) {file=$NF}}; if(/\[Input\]/) {input=1}} END {print path "/" file}' io_snowpack_base.ini)
+    begin=$(awk '{if(/\[DATA\]/) {getline; print $1; exit}}' ${metfile})
+    end=$(tail -n 1 ${metfile} | awk '{print $1}')
+    # Take care of specific settings to run at the requested time resolution:
+    echo "IMPORT_BEFORE = io_snowpack_base.ini" > io_snowpack_${ts}.ini
+    echo "[Input]" >> io_snowpack_${ts}.ini
+    echo "STATION1	= WFJ2_${ts}s.smet" >> io_snowpack_${ts}.ini
+    echo "[Output]" >> io_snowpack_${ts}.ini
+    echo "EXPERIMENT = output_${ts}s" >> io_snowpack_${ts}.ini
+    echo "[Snowpack]" >> io_snowpack_${ts}.ini
+    echo ${ts} | awk '{printf "CALCULATION_STEP_LENGTH = %f\n", $1/60.}' >> io_snowpack_${ts}.ini
+    echo "[Filters]" >> io_snowpack_${ts}.ini
+    echo "PSUM::arg1::cst         = ${ts}" >> io_snowpack_${ts}.ini
+    echo "[Interpolations1D]" >> io_snowpack_${ts}.ini
+    echo "PSUM::accumulate::period = ${ts}" >> io_snowpack_${ts}.ini
+    # Run SNOWPACK:
+    ../../snowpack/bin/snowpack -c io_snowpack_${ts}.ini -b ${begin} -e ${end}
 }
 
 
--- tests/temporaldownscaling.sh
+++ tests/temporaldownscaling.sh
@@ -12,6 +12,10 @@ begin=$(awk '(NR==2) {print $1 "T" $2}' ${metfile})
 end=$(awk 'END {print $1 "T" $2}' ${metfile})
 stn_id=$(fgrep CSV1_ID *ini | awk '{gsub(/ /, "", $NF); print $NF}')
 
+# Get WFJ2 IMIS data
+if [ ! -e "WFJ2.csv" ]; then
+	wget https://measurement-data.slf.ch/imis/data/by_station/WFJ2.csv
+fi
 
 #
 # Execute MeteoIO's meteoio_timeseries to interpolate temporally for each target_resolution
@@ -27,5 +31,11 @@ do
 
 	# Convert output file from meteoio_timseries to inp file for nix
 	awk -f convert_smet_to_inp.awk ${stn_id}.smet > WFJ_forcing_${target_resolution}s.txt
-	rm ${stn_id}.smet io.ini
+	rm io.ini
+	# HACK: Ideally, we would just get an output file that contains everything, and we could use the line below
+	#mv ${stn_id}.smet SNOWPACK/${stn_id}_${target_resolution}s.smet
+	# However, timestamps appear doubled (https://gitlabext.wsl.ch/snow-models/meteoio/-/issues/936). Until this is fixed, the lines below take care of the doubled timestamps.
+	awk 'BEGIN {header=0; data=0;} {if(!data) {print}; if(data) {d++; if(d%2==1) print}; if(/SMET 1.1 ASCII/) {header=1; data=0}; if(/\[DATA\]/) {header=0; data=1}}' ${stn_id}.smet > SNOWPACK/${stn_id}_${target_resolution}s.smet
+	awk 'BEGIN {header=0; data=0;} {if(!data) {print}; if(data) {d++; if(d%2==0) print}; if(/SMET 1.1 ASCII/) {header=1; data=0}; if(/\[DATA\]/) {header=0; data=1}}' ${stn_id}.smet > SNOWPACK/${stn_id}_${target_resolution}s_validation.smet
+	rm ${stn_id}.smet
 done
