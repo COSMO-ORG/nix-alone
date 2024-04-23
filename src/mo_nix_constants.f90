510cca4d47bafd31bcfb081d9492b8d43f688184
Nander Wever <nander.wever@meteoswiss.ch>
2024-04-23 10:22:21 +0200

    Unifying mo_nix_constants.f90

--- src/mo_nix_constants.f90
+++ src/mo_nix_constants.f90
@@ -4,14 +4,14 @@
 !! -------------------------------------------------------------------
 !!
 !! @par Description:
-!!  This module ...
+!!  This module ... 
 !!
-!! @author:
+!! @author: 
 !!
 !! @par Reference ADD PUBLICATIONS ONCE AVAILABLE
 !!
 !! @par Revision History
-!!
+!!  
 !!
 !! @par Copyright and License
 !!  This code is subject to the DWD and MPI-M-Software-License-Agreement in
@@ -29,13 +29,15 @@
 
 MODULE mo_nix_constants
 
-   USE mo_kind,                    ONLY: wp
+  USE mo_kind,                    ONLY: wp
 
 ! ------------------------------------------------------------------------------
 ! DECLARATIONS
 ! ------------------------------------------------------------------------------
 
-   IMPLICIT NONE
+IMPLICIT NONE
+
+! PRIVATE
 
 !------------------------------------------------------------------------------
 ! Anything public?
@@ -43,32 +45,33 @@ MODULE mo_nix_constants
 
    PUBLIC      ! All constants and variables in this module are public
 
+  ! 1. Physical constants  NOTE: a bunch of these already exists globally, but
+  !                              might have slithly different values so double check and replace at some point
+  ! -------------------------------------------------
 
+  REAL  (KIND=wp), PARAMETER           ::  &
 
+    rho_i                       = 917.0_wp               , & ! > density of ice                 (kg/m**3)
+    rho_a                       = 1.1_wp                 , & ! > density of air                 (kg/m**3)
 
+    theta_s                     = 1.0_wp                 , & ! > Saturated Water Content, for now we say 1.0
+    theta_r                     = 0.0_wp                 , & ! > Minimum amount of liquid water that will remain
 
-   ! 1. Physical constants  NOTE: a bunch of these already exists globally, but
-   !                              might have slithly different values so double check and replace at some point
-   ! -------------------------------------------------
+    e_snow                      = 0.98_wp                , & ! > Long-wave emissivity for snow
 
-   REAL  (KIND=wp), PARAMETER           ::  &
+    min_newsnow_layer           = 0.002_wp               , & ! > minimum new snow layer thickness, precipitation below this threshold
+                                                             !   is kept in storage, until sufficient precipitation has accumulated to
+                                                             !   build min_newsnow_layer (m)
 
-      rho_i                       = 917.0_wp               , & ! density of ice                 (kg/m**3)
-      rho_a                       = 1.1_wp                 , & ! density of air                 (kg/m**3)
+    specific_heat_air           = 1004.67_wp             , & !
+    specific_heat_ice           = 2100.0_wp              , & !
+    specific_heat_water         = 4190.0_wp              , & !
 
-      specific_heat_air           = 1004.67_wp             , & !
-      specific_heat_ice           = 2100.0_wp              , & !
-      specific_heat_water         = 4190.0_wp              , & !
+    ctalb                       = 0.004_wp                   ! VS : added snow physical constants
 
-      theta_s                     = 1.0_wp                 , & ! Saturated Water Content, for now we say 1.0
-      theta_r                     = 0.0_wp                 , & ! Minimum amount of liquid water that will remain
-      ctalb                       = 0.004_wp               , & ! VS : added snow physical constants
+    REAL  (KIND=wp), PARAMETER :: eps_div  = 1.0E-6_wp
 
-      e_snow                      = 0.98_wp                    ! lw emissivity for snow
-
-   REAL  (KIND=wp), PARAMETER :: eps_div  = 1.0E-6_wp
-
-   INTEGER , PARAMETER :: itype_heatcond = 1
+    INTEGER , PARAMETER :: itype_heatcond = 1
 !------------------------------------------------------------------------------
 ! End of module mo_nix_blanc
 !------------------------------------------------------------------------------
